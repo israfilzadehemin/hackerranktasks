6ab06d00b1fed52afb409a9cceed61e9417cd9ca
eminisra <israfilzadehemin@gmailcom>
2024-04-16 22:30:16 +0200

    SaveThePrisoner

---
+++ src/main/java/app/SaveThePrisoner.java
@@ -0,0 +1,17 @@
+package app;
+
+public class SaveThePrisoner {
+    public static void main(String[] args) {
+        System.err.println(saveThePrisoner(352926151, 380324688, 94730870));
+    }
+
+
+    public static int saveThePrisoner(int n, int m, int s) {
+
+        int unlucky = (m + s - 1) % n;
+
+        return unlucky == 0 ? n : unlucky;
+
+    }
+
+}
